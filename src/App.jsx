cec3d7170333f677a9dd6434a17664ab9682b9ec
IPogorelova <innaspbsu@gmail.com>
2025-08-16 17:57:45 +0200

    Add Clock

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { linksData } from './components/links-data.js';
 import { Yarlik, YarlikButton } from './components/yarlik/index.jsx';
 import Notebook from './components/notebook/index.jsx';
 import StartButton from './components/start-button/index.jsx';
+import Clock from './components/clock/index.jsx';
 
 import notebookIcon from './assets/images/icons/notebook.png';
 
@@ -17,10 +18,17 @@ const App = () => {
       <h1 className="visually-hidden">The page about "Released!" podcast</h1>
       <ul className="desktop-container">
         {linksData.map(link => <li key={link.text}><Yarlik {...link} /></li>)}
-        <li><YarlikButton text="Notebook" icon={notebookIcon} onClick={() => setIsNotebookOpened(true)} /></li>
+        <li>
+          <YarlikButton
+            text="Notebook"
+            icon={notebookIcon}
+            onClick={() => setIsNotebookOpened(true)}
+          />
+        </li>
       </ul>
       <Notebook isOpened={isNotebookOpened} setIsOpened={setIsNotebookOpened} />
       <StartButton />
+      <Clock />
     </>
   )
 };
---
+++ src/components/clock/index.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+
+import './index.css';
+
+const Clock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  const formatTime = (date) => {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    hours = hours < 10 ? `0${hours}` : hours;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+
+    return `${hours}:${minutes}`;
+  };
+
+  return (
+    <p className="clock">
+      {formatTime(time)}
+    </p>
+  )
+};
+
+export default Clock;
